bb9a771eb1ba52f6cc0303d33680ecd9dd2ad9ba
rukonpro <rukon.js@gmail.com>
2025-05-19 23:31:28 +0600

    api change

--- src/Hooks/Api.js
+++ src/Hooks/Api.js
@@ -1,5 +1,7 @@
 // const api1 = `http://localhost:8000/api`;
 
 // const api1 = `https://eskul-server.herokuapp.com/api`;
-const api1 = `https://eskul-server.onrender.com/api`;
+// const api1 = `https://eskul-server.onrender.com/api`;
+const api1 = `https://eskul-server.vercel.app/api`;
 export const api = api1;
+
